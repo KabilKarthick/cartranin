535e03c15675689ba30cac2e35642ce19951e81e
kabilkarhick <kabilkbp@gmail.com>
2025-04-17 11:25:40 +0530

    first commit

---
+++ src/main/java/com/example/tring/TringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tring/TringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
